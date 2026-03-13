a8b3bb1ab8430fdc11a214ba3c4ed58a33d18085
TheMedTimes <arham.college.req@gmail.com>
2026-03-13 17:32:32 +0530

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,47 @@
+const CACHE_NAME = 'themedtimes-v1';
+const ASSETS = [
+  '/The_Med_Times/',
+  '/The_Med_Times/index.html',
+  '/The_Med_Times/manifest.json',
+  '/The_Med_Times/icons/icon-192x192.png',
+  '/The_Med_Times/icons/icon-512x512.png',
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  const url = new URL(event.request.url);
+
+  if (url.pathname.includes('/puzzles/')) {
+    event.respondWith(
+      fetch(event.request).catch(() => caches.match(event.request))
+    );
+    return;
+  }
+
+  event.respondWith(
+    caches.match(event.request).then(cached => {
+      return cached || fetch(event.request).then(response => {
+        if (response.ok) {
+          const clone = response.clone();
+          caches.open(CACHE_NAME).then(cache => cache.put(event.request, clone));
+        }
+        return response;
+      });
+    })
+  );
+});
